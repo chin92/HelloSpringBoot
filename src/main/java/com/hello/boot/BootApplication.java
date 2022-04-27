c50c31d514ec98e8eba6c97383b8ad93f52d0e2a
chin <contact.chinthana@gmail.com>
2022-04-27 14:18:36 -0500

    added test code

--- src/main/java/com/hello/boot/BootApplication.java
+++ src/main/java/com/hello/boot/BootApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootApplication {
 
+	//test
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
